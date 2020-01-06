380e79ba16035155447217066047bd0d6961a0cf
ManeevDumitru <shanegrimes@mail.ru>
2020-01-06 13:47:13 +0200

    Features added

--- js/main.js
+++ js/main.js
@@ -12,19 +12,31 @@ function BH() {
     $(buttonOpenBH).click(function(){
         $('#backgroundBH').fadeIn(500);
         $(body).fadeTo(500, .2);
+        $('body').css({
+            'overflow': 'hidden'
+        })
     });
     $(buttonOpenTR).click(function(){
-        $('#backgroundBH').fadeIn(500);
+        $('#backgroundTR').fadeIn(500);
         $(body).fadeTo(500, .2);
+        $('body').css({
+            'overflow': 'hidden'
+        })
     });
     $(buttonOpenCO).click(function(){
-        $('#backgroundBH').fadeIn(500);
+        $('#backgroundCO').fadeIn(500);
         $(body).fadeTo(500, .2);
+        $('body').css({
+            'overflow': 'hidden'
+        })
     });
     $(buttonClose).click(function(){
         $('#backgroundBH').fadeOut(500);
-        /*$('#backgroundTR').fadeOut(500);
-        $('#backgroundCO').fadeOut(500);*/
-        $(body).fadeTo(500, 1)
+        $('#backgroundTR').fadeOut(500);
+        $('#backgroundCO').fadeOut(500);
+        $(body).fadeTo(500, 1);
+        $('body').css({
+            'overflow': 'visible'
+        })
     });
 }
