367fcb079c2493a43e3b4eb326c850ccfece1e57
ManeevDumitru <shanegrimes@mail.ru>
2020-01-16 21:27:59 +0200

    DONE

--- js/main.js
+++ js/main.js
@@ -4,33 +4,33 @@ $(document).ready(function () {
 });
 
 function BH() {
-    let buttonOpenBH = document.getElementById('buttonBH');
-    let buttonOpenTR = document.getElementById('buttonTR');
-    let buttonOpenCO = document.getElementById('buttonCO');
+    let buttonOpenBH = document.getElementsByClassName('buttonBH');
+    let buttonOpenTR = document.getElementsByClassName('buttonTR');
+    let buttonOpenCO = document.getElementsByClassName('buttonCO');
     let buttonClose = document.getElementsByClassName('buttonClose');
     let body = document.getElementsByTagName('section');
-    $(buttonOpenBH).click(function(){
+    $(buttonOpenBH).click(function () {
         $('#backgroundBH').fadeIn(500);
         $(body).fadeTo(500, .2);
         $('body').css({
             'overflow': 'hidden'
         })
     });
-    $(buttonOpenTR).click(function(){
+    $(buttonOpenTR).click(function () {
         $('#backgroundTR').fadeIn(500);
         $(body).fadeTo(500, .2);
         $('body').css({
             'overflow': 'hidden'
         })
     });
-    $(buttonOpenCO).click(function(){
+    $(buttonOpenCO).click(function () {
         $('#backgroundCO').fadeIn(500);
         $(body).fadeTo(500, .2);
         $('body').css({
             'overflow': 'hidden'
         })
     });
-    $(buttonClose).click(function(){
+    $(buttonClose).click(function () {
         $('#backgroundBH').fadeOut(500);
         $('#backgroundTR').fadeOut(500);
         $('#backgroundCO').fadeOut(500);
